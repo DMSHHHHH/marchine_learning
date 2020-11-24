d5d7000f9704651889d4e2e568de55eaa9f4781e
dhmss <dhmss@g2.com>
2020-11-24 11:32:04 +0800

    changes

--- transformers/tensorflow/test.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf 
-from config_gpt2 import GPT2Config
-from gpt2SentencePretrain import GPT2Model
-config = GPT2Config.from_pretrained(pretrained_model_name_or_path)
-model = GPT2Model(config)
